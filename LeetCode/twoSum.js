540c34dfc0cd93b7b1b67b5d29bf4545565e3967
NehemiasSalazar <75285606+nehemias1999@users.noreply.github.com>
2023-02-27 21:39:33 -0300

    Add files via upload

---
+++ LeetCode/twoSum.js
@@ -0,0 +1,64 @@
+
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+
+    2 <= nums.length <= 10^4
+    -10^9 <= nums[i] <= 10^9
+    -10^9 <= target <= 10^9
+    Only one valid answer exists.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+    
+    if(nums.length === 2) 
+        return [0, 1];
+
+    let numbers = new Map();
+    let index = 0;
+
+    while(index < nums.length) {
+        let element = nums[index];
+
+        if(numbers.has(target - element))
+            return [numbers.get(target - element), index];
+
+        numbers.set(element, index);
+
+        index++;
+    }
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(nums, target));
